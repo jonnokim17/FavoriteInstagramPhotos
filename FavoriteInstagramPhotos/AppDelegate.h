e619349b060cbbb3c9966bd50198e8b0bff620e1
Jonathan Kim <jonnokim17@gmail.com>
2014-11-08 14:12:32 -0800

    Initial Commit

---
+++ FavoriteInstagramPhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FavoriteInstagramPhotos
+//
+//  Created by Jonathan Kim on 11/8/14.
+//  Copyright (c) 2014 Jonathan Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
